72a15b9e1743006eab8a30f6c089617ab53b8f52
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2024-05-04 00:33:21 +0100

    Add more basic syntax splitting tests

--- src/tests/tokenize.test.ts
+++ src/tests/tokenize.test.ts
@@ -14,8 +14,10 @@ describe('tokenizing note letters', (): void => {
         ['PACE', 'P'],
     ])(
         'tokenizing %s throws RangeError because %s is not in the musical alphabet',
-        (inputString): void => {
-            expect((): string[] => tokenize(inputString)).toThrow(RangeError);
+        (inputString, badChar): void => {
+            expect((): string[] => tokenize(inputString)).toThrow(
+                new RangeError(`${badChar} is not a valid MML character.`)
+            );
         }
     );
 });
@@ -29,18 +31,50 @@ describe('tokenizing note letters with attached durations', (): void => {
     });
 });
 
+describe('handling non-tokenizable operators/modifiers', (): void => {
+    it('does not tokenize valid MML characters that are not related to individual notes', (): void => {
+        expect(tokenize('T100L4V10O5<>')).toEqual([]);
+        expect(tokenize('T180LA4C16<DC8>V8T130')).toEqual(['C16', 'D', 'C8']);
+    });
+});
+
 describe('tokenizing with accidentals', (): void => {
     it('treats both # and + as sharp signs', (): void => {
         expect(tokenize('A#A+')).toEqual(['A#', 'A#']);
     });
 
     it.each([
-        [
-            'A+2B#2C4D-1E+4F1G-2',
-            ['A#2', 'B#2', 'C4', 'D-1', 'E#4', 'F1', 'G-2'],
-        ],
+        ['A+2B#2C4D-E+4F1G-2', ['A#2', 'B#2', 'C4', 'D-', 'E#4', 'F1', 'G-2']],
         ['B-8A-16G+32', ['B-8', 'A-16', 'G#32']],
     ])('tokenizes %s into %p', (inputString, outputArray): void => {
         expect(tokenize(inputString)).toEqual(outputArray);
+    });
+
+    it('throws RangeError if accidental not attached to a note letter', (): void => {
+        const AccidentalError = new RangeError(
+            `Accidentals must be preceded by a note letter.`
+        );
+        expect((): string[] => tokenize('C4#')).toThrow(AccidentalError);
+        expect((): string[] => tokenize('B#2C4.-')).toThrow(AccidentalError);
+        expect((): string[] => tokenize('C##')).toThrow(AccidentalError);
+    });
+});
+
+describe('tokenizing durations with dots', (): void => {
+    it.each([
+        ['A2.C#4G-.F32.', ['A2.', 'C#4', 'G-.', 'F32.']],
+        ['B16..C4.G8..', ['B16..', 'C4.', 'G8..']],
+    ])('tokenizes %s into %p', (inputString, outputArray): void => {
+        expect(tokenize(inputString)).toEqual(outputArray);
+    });
+
+    it('throws RangeError if dot not attached to a duration', (): void => {
+        const DotError = new RangeError(
+            `Dots must be preceded by a note duration or another dot.`
+        );
+        expect((): string[] => tokenize('A.')).toThrow(DotError);
+        expect((): string[] => tokenize('B4.C..')).toThrow(DotError);
+        expect((): string[] => tokenize('T120.')).toThrow(DotError);
+        expect((): string[] => tokenize('L8.')).toThrow(DotError);
     });
 });
